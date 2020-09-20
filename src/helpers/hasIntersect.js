67c6ba7a72ead6b4e53e254f9158deebad902280
erye <erye@tiiit.cn>
2020-09-21 00:23:22 +0800

    修复hasIntersect方法中&&错误写作&的问题

--- src/helpers/hasIntersect.js
+++ src/helpers/hasIntersect.js
@@ -1,6 +1,6 @@
 // 2个数组是否有交集
 export default function hasIntersect(a, b) {
-  if (Array.isArray(a) & Array.isArray(b)) {
+  if (Array.isArray(a) && Array.isArray(b)) {
     for (const item of a) {
       if (b.includes(item)) return true;
     }
